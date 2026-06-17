9843b1860c974948517e8021dc3cfd35b77eb7b9
veer-trading-bgk <algonekar@gmail.com>
2026-06-17 20:20:23 +0530

    Fix: absolute path for global.css in metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,8 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withNativeWind(config, {
+  input: path.resolve(__dirname, 'global.css'),
+});
